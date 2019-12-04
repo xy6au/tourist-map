1b540ef4c5e3c23b8923cae05b91ce0bc97f14bc
xy6au <56407106+xy6au@users.noreply.github.com>
2019-12-04 00:13:53 -0500

    initial

---
+++ map.js
@@ -0,0 +1,331 @@
+// Global variables
+var mapCenter = [-122.25573, 37.80397];
+var mapZoom = 11.74;
+
+
+// --------------------------------------------------------
+// 1. Initialize map
+    mapboxgl.accessToken = 'pk.eyJ1IjoieHk2YXUiLCJhIjoiY2sxdXlwejk5MGZ5aTNncXV3MGx0Y3JmcCJ9.2LvX99rC6vL08hPV1pVhxg'; // replace this value with your own access token from Mapbox Studio
+
+    // for more mapboxgl.Map options, see https://docs.mapbox.com/mapbox-gl-js/api/#map)
+    var map = new mapboxgl.Map({
+    	container: 'map', // this is the ID of the div in index.html where the map should go
+        center: mapCenter, // set the centerpoint of the map programatically. Note that this is [longitude, latitude]!
+        zoom: mapZoom, // set the default zoom programatically
+    	style: 'mapbox://styles/xy6au/ck3qmcs881fp31cmz1q51q14o', // replace this value with the style URL from Mapbox Studio
+    	customAttribution: 'City of Charlottesville Open Data Portal (http://opendata.charlottesville.org/)', // Custom text used to attribute data source(s)
+    });
+
+
+// --------------------------------------------------------
+// 2. Show a modal window when About button is clicked
+// A modal window is an element that sits on top of an application's main window. It can be opened and closed without reloading the page
+
+    $("#about").on('click', function() { // Click event handler for the About button in jQuery, see https://api.jquery.com/click/
+        $("#screen").fadeToggle(); // shows/hides the black screen behind modal, see https://api.jquery.com/fadeToggle/
+        $(".modal").fadeToggle(); // shows/hides the modal itself, see https://api.jquery.com/fadeToggle/
+    });
+
+    $(".modal>.close-button").on('click', function() { // Click event handler for the modal's close button
+        $("#screen").fadeToggle();
+        $(".modal").fadeToggle();
+    });
+
+// --------------------------------------------------------
+// 3. Creating a legend
+// See example tutorial at https://docs.mapbox.com/help/tutorials/choropleth-studio-gl-pt-2/#create-arrays-of-intervals-and-colors
+
+    var layers = [ // an array of the possible values you want to show in your legend
+        'Civic Spaces', // Civic Spaces.png
+        'Community Park', // Community Park.png
+        'Neighborhood Park', // Neighborhood Park.png
+        'Cemetery',
+        'Urban Park',
+        'Regional Park'
+    ];
+
+    var colors = [ // an array of the color values for each legend item
+        '#800000',
+        '#800030',
+        '#800060',
+        '#80006c',
+        '#800090',
+        '#80009c'
+    ];
+
+    // for loop to create individual legend items
+    for (i=0; i<layers.length; i++) {
+        var layer =layers[i]; // name of the current legend item, from the layers array
+        var color =colors[i]; // color value of the current legend item, from the colors array 
+        
+        var itemHTML = "<div><span class='legend-key'></span><span>" + layer + "</span></div>"; // create the HTML for the legend element to be added
+
+        var item = $(itemHTML).appendTo("#legend"); // add the legend item to the legend
+        var legendKey = $(item).find(".legend-key"); // find the legend key (colored circle) for the current item
+        legendKey.css("background", color); // change the background color of the legend key
+    }
+
+
+// --------------------------------------------------------
+// 4. Info window 
+// See example tutorial at https://docs.mapbox.com/help/tutorials/choropleth-studio-gl-pt-2/#add-the-information-window
+
+    map.on('mousemove', function(e) {   // Event listener to do some code when the mouse moves, see https://www.mapbox.com/mapbox-gl-js/api/#events. 
+
+        var parks = map.queryRenderedFeatures(e.point, {    
+            layers: ['cville-parks']    // replace 'cville-parks' with the name of the layer you want to query (from your Mapbox Studio map, the name in the layers panel). For more info on queryRenderedFeatures, see the example at https://www.mapbox.com/mapbox-gl-js/example/queryrenderedfeatures/. Documentation at https://www.mapbox.com/mapbox-gl-js/api/#map#queryrenderedfeatures.
+        });
+              
+        if (parks.length > 0) {   // if statement to make sure the following code is only added to the info window if the mouse moves over a state
+
+            $('#info-window-body').html('<h3><strong>' + parks[0].properties.PARKNAME + '</strong></h3><p>' + parks[0].properties.PARK_TYPE + ' PARK</p><img class="park-image" src="img/' + parks[0].properties.PARKNAME + '.jpg">');
+
+        } else {    // what shows up in the info window if you are NOT hovering over a park
+
+            $('#info-window-body').html('<p>Find your favorite attractions in Oakland!');
+            
+        }
+
+    });
+
+
+// -------------------------------------------------------- 
+// 5. Popups
+// See tutorial at https://docs.mapbox.com/help/tutorials/add-points-pt-3/
+// See example of popups on click at https://docs.mapbox.com/mapbox-gl-js/example/popup-on-click/ 
+// See example of popups on hover at https://docs.mapbox.com/mapbox-gl-js/example/popup-on-hover/
+
+    // Create a popup on click 
+    map.on('click', function(e) {   // Event listener to do some code when user clicks on the map
+
+      var stops = map.queryRenderedFeatures(e.point, {  // Query the map at the clicked point. See https://www.mapbox.com/mapbox-gl-js/example/queryrenderedfeatures/ for an example on how queryRenderedFeatures works and https://www.mapbox.com/mapbox-gl-js/api/#map#queryrenderedfeatures for documentation
+        layers: ['cville-bus-stops']    // replace this with the name of the layer from the Mapbox Studio layers panel
+    });
+
+      // if the layer is empty, this if statement will exit the function (no popups created) -- this is a failsafe to avoid non-functioning popups
+      if (stops.length == 0) {
+        return;
+    }
+
+    // Initiate the popup
+    var popup = new mapboxgl.Popup({ 
+        closeButton: true, // If true, a close button will appear in the top right corner of the popup. Default = true
+        closeOnClick: true, // If true, the popup will automatically close if the user clicks anywhere on the map. Default = true
+        anchor: 'bottom', // The popup's location relative to the feature. Options are 'top', 'bottom', 'left', 'right', 'top-left', 'top-right', 'bottom-left' and 'bottom-right'. If not set, the popup's location will be set dynamically to make sure it is always visible in the map container.
+        offset: [0, -15] // A pixel offset from the centerpoint of the feature. Can be a single number, an [x,y] coordinate, or an object of [x,y] coordinates specifying an offset for each of the different anchor options (e.g. 'top' and 'bottom'). Negative numbers indicate left and up.
+    });
+
+      // Set the popup location based on each feature
+      popup.setLngLat(stops[0].geometry.coordinates);
+
+      // Set the contents of the popup window
+      popup.setHTML('<h3>Stop ID: ' + stops[0].properties.stop_id + '</h3><p>' + stops[0].properties.stop_name + '</p>');
+            // stops[0].properties.stop_id will become the title of the popup (<h3> element)
+            // stops[0].properties.stop_name will become the body of the popup
+
+
+        // popup.setHTML('<p>' + stops[0].properties.stop_name + '</p>')
+        
+
+      // Add the popup to the map 
+      popup.addTo(map);  // replace "map" with the name of the variable in line 4, if different
+  });
+
+
+// -------------------------------------------------------- 
+// 6. Show/hide layers
+// See example at https://www.mapbox.com/mapbox-gl-js/example/toggle-layers/
+    
+    var layers = [  // an array of the layers you want to include in the layers control (layers to turn off and on)
+
+        // [layerMachineName, layerDisplayName]
+        // layerMachineName is the layer name as written in your Mapbox Studio map layers panel
+        // layerDisplayName is the way you want the layer's name to appear in the layers control on the website
+        ['cville-bus-stops', 'Bus Stops'],                      // layers[0]
+        ['cville-parks', 'Parks'],                              // layers[1][1] = 'Parks'
+        ['cville-bike-lanes', 'Bike Lanes'],     
+        ['cville-bus-stops-heatmap', 'Bus Stop Heatmap'],
+        ['background', 'Map background']
+        // add additional live data layers here as needed
+    ]; 
+
+    // functions to perform when map loads
+    map.on('load', function () {
+        
+        
+        for (i=0; i<layers.length; i++) {
+
+            // add a button for each layer
+            $("#layers-control").append("<a href='#' class='active button-default' id='" + layers[i][0] + "'>" + layers[i][1] + "</a>"); // see http://api.jquery.com/append/
+        }
+
+        // show/hide layers when button is clicked
+        $("#layers-control>a").on('click', function(e) {
+
+                var clickedLayer = e.target.id;
+
+                e.preventDefault();
+                e.stopPropagation();
+
+                var visibility = map.getLayoutProperty(clickedLayer, 'visibility');  // see https://www.mapbox.com/mapbox-gl-js/api/#map#getlayoutproperty
+                console.log(visibility);
+
+                if (visibility === 'visible') {
+                    map.setLayoutProperty(clickedLayer, 'visibility', 'none');  // see https://www.mapbox.com/mapbox-gl-js/api/#map#setlayoutproperty
+                    $(e.target).removeClass('active');
+                } else {
+                    $(e.target).addClass('active');
+                    map.setLayoutProperty(clickedLayer, 'visibility', 'visible'); // see https://www.mapbox.com/mapbox-gl-js/api/#map#setlayoutproperty
+                }
+        });
+    });
+
+// -------------------------------------------------------- 
+// 7. Change a layer's style
+// See example at https://www.mapbox.com/mapbox-gl-js/example/color-switcher/
+    
+    var swatches = $("#swatches");
+
+    var colors = [  // an array of color options for the bus stop ponts
+        '#ffd000',
+        '#f00',
+    ]; 
+
+    var layer = 'cville-bus-stops';
+
+    colors.forEach(function(color) {
+        var swatch = $("<button class='swatch'></button>").appendTo(swatches);
+
+        $(swatch).css('background-color', color); 
+
+        $(swatch).on('click', function() {
+            map.setPaintProperty(layer, 'circle-color', color); // 'circle-color' is a property specific to a circle layer. Read more about what values to use for different style properties and different types of layers at https://www.mapbox.com/mapbox-gl-js/style-spec/#layers
+        });
+
+        $(swatches).append(swatch);
+    });
+
+// -------------------------------------------------------- 
+// 8. Scroll to zoom through sites
+// See example at https://docs.mapbox.com/mapbox-gl-js/example/scroll-fly-to/
+    
+    // A JavaScript object containing all of the data for each site "chapter" (the sites to zoom to while scrolling)
+
+var chapters = {
+        'chinatown': {
+            name: "China town",
+            description: "One of the oldest Chinatowns in the United States.  Oakland Chinatown was settled in the 1850s, shortly after the gold rush.  In 1906, the San Francisco earthquake and fire wrecked San Francisco Chinatown, causing thousands of San Francisco Chinese survivors to move to Oakland to rebuild their lives in the East Bay.Oakland Chinatown nowadays becomes the center of Asian cultures, arts and gourmet cuisine.  You can find up to 8 Asian languages of books in the Oakland Public Library Asian Branch; learn Balinese dance at the Oakland Asian Cultural Center; eat Japanese Ramen and Vietnamese banh mi sandwich, and drink Taiwanese boba tea; and buy American Chinese fortune cookies which all can be found in Chinatown!",
+            imagepath: "img/chinatown.jpg",
+            center: [-122.27027, 37.79820],
+            zoom: 16.94,
+            pitch: 52.50,
+            bearing: 152.03
+        },
+        'lakemerritt': {
+            name: "Lake Merritt",
+            description: "Surrounded by luxuriant trees and verdant lawns, Lake Merritt is an idyllic parkland in the heart of the city of Oakland. Because the lake is a tidal lagoon filled with seawater, it is home to a marvelous variety of bird life such as Canadian geese, pelicans, the snowy egret, and black cormorants. Established in 1870, this is also the oldest designated wildlife refuge in the country.Surrounding the lake is Lakeside Park, where several acres of green space and a 3.2-mile scenic path for walking and jogging encircle the lake. Couples can take a romantic gondola ride across the lake, and the tranquil scenery is also perfect for picnics. To enjoy a more elegant meal, try the Lakeside Chalet, which has gorgeous views of the lake and music concerts on the dock during summertime.",
+            imagepath: "img/california-oakland-lake-merritt.jpg",
+            center: [-122.26135, 37.80280],
+            zoom: 14.91,
+            pitch: 55.50,
+            bearing: -0.38
+        },
+        'jactlondonsquare': {
+            name: "Jack London Square",
+            description: "In a picturesque setting on the Oakland estuary, Jack London Square has a relaxing maritime atmosphere. This historic neighborhood was a stomping ground of famous American author Jack London, who worked at the docks of the Oakland port. In the center of the square stands a replica of the log cabin where he lived during a wilderness expedition in Alaska. Today, Jack London Square is popular for dining or strolling along the boardwalk and waterfront trail. The area has many inviting restaurants along the European-style walkways, with waterfront terraces overlooking the marina.",
+            imagepath: "img/jordon.jpg",
+            center: [-122.27879, 37.79505],
+            zoom: 16.96,
+            pitch: 60.00,
+            bearing: -0.63
+        },
+        'redwoodregionpark': {
+            name: "Redwood Regional Park",
+            description: "This pristine redwood forest is just a few miles outside of downtown Oakland, and it's worth taking the drive out here to meditate in the redwood groves. Many of the stately coastal redwood trees (sequoia sempervirens) soar to 150 feet. This area was once cleared by logging for timber, but the forest has been replaced and is now protected parkland with hiking trails.",
+            imagepath: "img/redwood.jpg",
+            center: [-122.16695, 37.81629],
+            zoom: 14.30,
+            pitch: 60.00,
+            bearing: 8.80
+        }
+
+    };
+
+    console.log(chapters['chinatown']['name']);
+    console.log(Object.keys(chapters)[0]);
+
+    // Add the chapters to the #chapters div on the webpage
+    for (var key in chapters) {
+        var newChapter = $("<div class='chapter' id='" + key + "'></div>").appendTo("#chapters");
+        var chapterHTML = $("<h3>" + chapters[key]['name'] + "</h3><img src='" + chapters[key]['imagepath'] + "'><p>" + chapters[key]['description'] + "</p>").appendTo(newChapter);
+    }
+
+
+    $("#chapters").scroll(function(e) {
+
+        var chapterNames = Object.keys(chapters);
+
+        for (var i = 0; i < chapterNames.length; i++) {
+
+            var chapterName = chapterNames[i];
+            var chapterElem = $("#" + chapterName);
+
+            if (chapterElem.length) {
+
+                if (checkInView($("#chapters"), chapterElem, true)) {
+                    setActiveChapter(chapterName);
+                    $("#" + chapterName).addClass('active');
+
+                    break;
+
+                } else {
+                    $("#" + chapterName).removeClass('active');
+                }
+            }
+        }
+    });
+
+    var activeChapterName = '';
+    
+    function setActiveChapter(chapterName) {
+        if (chapterName === activeChapterName) return;
+
+        map.flyTo(chapters[chapterName]);
+
+        activeChapterName = chapterName;
+    }
+
+    function checkInView(container, elem, partial) {
+        var contHeight = container.height();
+        var contTop = container.scrollTop();
+        var contBottom = contTop + contHeight ;
+
+        var elemTop = $(elem).offset().top - container.offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+
+        var isTotal = (elemTop >= 0 && elemBottom <=contHeight);
+        var isPart = ((elemTop < 0 && elemBottom > 0 ) || (elemTop > 0 && elemTop <= container.height())) && partial ;
+
+        return  isTotal  || isPart ;
+    }
+    
+
+// -------------------------------------------------------- 
+// 9. Reset map button
+    
+    $("#reset").click(function() {
+        map.setCenter(mapCenter);
+        map.setZoom(mapZoom);
+        map.setPitch(0);
+        map.setBearing(0);
+        map.setFilter("cville-building-permits", null); // reset building permits filters
+        
+        // Reset all layers to visible
+        for (i=0; i<layers.length; i++) {
+            map.setLayoutProperty(layers[i][0], 'visibility', 'visible'); 
+            $("#" + layers[i][0]).addClass('active');
+        }                   
+
+    });
+
